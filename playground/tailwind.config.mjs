f22aca1fdac37c8df77a3c69b0761729ca0855df
Jacob Weinbren <jacobweinbren@gmail.com>
2024-05-17 21:47:36 +0100

    Made work with pnpm

--- playground/tailwind.config.mjs
+++ playground/tailwind.config.mjs
@@ -1,10 +1,12 @@
 import customPlugin from "primer-components/tailwind-plugin.js";
 
+// Check the environment variable
+const isDev = process.env.NODE_ENV === "development";
+
 export default {
 	content: [
 		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
 		"./node_modules/primer-components/**/*.{js,jsx,ts,tsx,astro}",
-		"../package/src/**/*.{js,jsx,ts,tsx,astro}",
 	],
 	theme: {
 		extend: {},
