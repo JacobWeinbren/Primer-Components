cd6931a7333aa08913faab1a26d4fefeb24c0c2b
Jacob Weinbren <jacobweinbren@gmail.com>
2024-05-17 21:27:20 +0100

    Got purging to work

--- playground/tailwind.config.mjs
+++ playground/tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
 	content: [
 		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
 		"./node_modules/primer-components/**/*.{js,jsx,ts,tsx,astro}",
-		"../primer-components/**/*.{js,jsx,ts,tsx,astro}",
+		"../package/src/**/*.{js,jsx,ts,tsx,astro}",
 	],
 	theme: {
 		extend: {},
