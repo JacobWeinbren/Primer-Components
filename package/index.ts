312a0ea616fb4e78275c585d0811af25ba179625
Jacob Weinbren <jacobweinbren@gmail.com>
2024-05-20 14:42:04 +0100

    Temporary ActionBar

--- package/index.ts
+++ package/index.ts
@@ -1,3 +1,7 @@
 import Button from "./src/components/Button.astro";
+import ActionBar from "./src/components/ActionBar.astro";
+import ActionBarItem from "./src/components/ActionBarItem.astro";
+import ActionBarDivider from "./src/components/ActionBarDivider.astro";
+import Icon from "./src/components/Icon.astro";
 
-export { Button };
+export { Button, ActionBar, ActionBarItem, ActionBarDivider, Icon };
